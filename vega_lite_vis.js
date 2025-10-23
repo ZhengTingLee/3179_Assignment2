15b9336575a4a4cde3d101ff8abd59e1d2ee6fb3
Lee Zheng Ting <160329310+ZhengTingLee@users.noreply.github.com>
2025-10-23 22:00:23 +0800

    Remove unuse code

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,18 +3,6 @@ var vg_2 = "bar_chart.vg.json";
 var vg_3 = "detail_bar_chart.vg.json";
 var vg_4 = "line_chart.vg.json";
 
-// Promise.all([
-//     vegaEmbed("#map", vg_1, {actions: false}), 
-//     vegaEmbed("#bar_chart", vg_2, {actions: false})
-//   ]).then(function(results) {
-//   const mapView = results[0].view;
-//   const barView = results[1].view;
-
-//   mapView.addSignalListener("Year_selection", function(name, value) {
-//     barView.signal("Year_selection", value).run();
-//   });
-
-// }).catch(console.error);
 vegaEmbed("#map", vg_1, {actions: false}).then(function(result) {}).catch(console.error);
 vegaEmbed("#bar_chart", vg_2, {actions: false}).then(function(result) {}).catch(console.error);
 vegaEmbed("#detail_bar_chart", vg_3, {actions: false}).then(function(result) {}).catch(console.error);
